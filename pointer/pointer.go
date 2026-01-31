c72f57842217305a7b021b4eeb88d056ba484d77
Petr Homola <homola.p@gmail.com>
2026-01-31 01:00:49 +0000

    pointer-to

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,7 +1,9 @@
 package pointer
 
+import "time"
+
 // To returns a pointer to its argument.
-func To[T any](x T) *T {
+func To[T string | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | complex64 | complex128 | time.Time](x T) *T {
 	return &x
 }
 
--- pointer/pointer_test.go
+++ pointer/pointer_test.go
@@ -2,13 +2,20 @@ package pointer
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestPointerTo(t *testing.T) {
 	req := require.New(t)
-
-	p := To("abcd")
-	req.Equal("abcd", *p)
+	{
+		p := To("abcd")
+		req.Equal("abcd", *p)
+	}
+	{
+		t := time.Now()
+		p := To(t)
+		req.Equal(t, *p)
+	}
 }
