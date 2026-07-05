600e4dbb9ec7ff91f33c7293a1af72efc835877c
Petr Homola <homola.p@gmail.com>
2026-07-05 11:55:47 +0100

    embellished

--- copier/copier.go
+++ copier/copier.go
@@ -9,7 +9,8 @@ import (
 )
 
 var (
-	copiers sync.Map
+	copiers  sync.Map
+	copiers2 sync.Map
 	// ErrInvalidType ...
 	ErrInvalidType = errors.New("invalid type")
 	// ErrFieldMissing ...
@@ -108,6 +109,109 @@ func Copy[D, S any](dst *D, src *S) error {
 func Copied[D, S any](src *S) (*D, error) {
 	var dst D
 	if err := Copy(&dst, src); err != nil {
+		return nil, err
+	}
+	return &dst, nil
+}
+
+func getCopier2(dst, src reflect.Type) (func(unsafe.Pointer, unsafe.Pointer) error, error) {
+	pair := typePair{dst, src}
+	if f, ok := copiers2.Load(pair); ok {
+		return f.(func(unsafe.Pointer, unsafe.Pointer) error), nil
+	}
+	if dst.Kind() != reflect.Struct || src.Kind() != reflect.Struct {
+		return nil, ErrInvalidType
+	}
+	fieldCopiers := make([]func(unsafe.Pointer, unsafe.Pointer) error, 0, src.NumField())
+	for fs := range src.Fields() {
+		if fs.Tag.Get("copier") == "-" {
+			continue
+		}
+		fd, ok := dst.FieldByName(fs.Name)
+		if !ok {
+			return nil, ErrFieldMissing
+		}
+		so := fs.Offset
+		do := fd.Offset
+		switch {
+		case fs.Type.Kind() == reflect.Int && fd.Type.Kind() == reflect.Int:
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				*(*int)(dst) = *(*int)(src)
+				return nil
+			})
+		case fs.Type.Kind() == reflect.Float64 && fd.Type.Kind() == reflect.Float64:
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				*(*float64)(dst) = *(*float64)(src)
+				return nil
+			})
+		case fs.Type.Kind() == reflect.String && fd.Type.Kind() == reflect.String:
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				*(*string)(dst) = *(*string)(src)
+				return nil
+			})
+		case fs.Type.Kind() == reflect.Pointer && fd.Type.Kind() == reflect.Pointer && fs.Type.Elem() == fd.Type.Elem():
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				*(*unsafe.Pointer)(dst) = *(*unsafe.Pointer)(src)
+				return nil
+			})
+		case fs.Type.Kind() == reflect.Struct && fd.Type.Kind() == reflect.Struct:
+			if fs.Type == fd.Type {
+				fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+					dst = unsafe.Add(dst, do)
+					src = unsafe.Add(src, so)
+					d := reflect.NewAt(fd.Type, dst)
+					s := reflect.NewAt(fs.Type, src)
+					d.Elem().Set(s.Elem())
+					return nil
+				})
+			} else {
+				c, err := getCopier(fd.Type, fs.Type)
+				if err != nil {
+					return nil, err
+				}
+				fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+					dst = unsafe.Add(dst, do)
+					src = unsafe.Add(src, so)
+					return c(dst, src)
+				})
+			}
+		default:
+			return nil, fmt.Errorf("can't create copier for %s -> %s", fs.Type, fd.Type)
+		}
+	}
+	f := func(dst, src unsafe.Pointer) error {
+		for _, c := range fieldCopiers {
+			if err := c(dst, src); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	copiers2.Store(pair, f)
+	return f, nil
+}
+
+// Copy2 ...
+func Copy2[D, S any](dst *D, src *S) error {
+	f, err := getCopier2(reflect.TypeFor[D](), reflect.TypeFor[S]())
+	if err != nil {
+		return err
+	}
+	return f(unsafe.Pointer(dst), unsafe.Pointer(src))
+}
+
+// Copied2 ...
+func Copied2[D, S any](src *S) (*D, error) {
+	var dst D
+	if err := Copy2(&dst, src); err != nil {
 		return nil, err
 	}
 	return &dst, nil
--- copier/copier_test.go
+++ copier/copier_test.go
@@ -20,6 +20,7 @@ type (
 		Height float64
 		IP     *s2
 		I      d2
+		I2     s2
 		X      bool
 	}
 	s struct {
@@ -28,6 +29,7 @@ type (
 		Height float64
 		IP     *s2
 		I      s2
+		I2     s2
 	}
 )
 
@@ -35,7 +37,7 @@ func TestCopy(t *testing.T) {
 	req := require.New(t)
 
 	var r d
-	err := Copy(&r, &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}})
+	err := Copy(&r, &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}, s2{"ijkl"}})
 	req.Nil(err)
 	req.Equal("name", r.Name)
 	req.Equal(1234, r.Age)
@@ -44,9 +46,23 @@ func TestCopy(t *testing.T) {
 	req.Equal("efgh", r.I.A)
 }
 
+func TestCopy2(t *testing.T) {
+	req := require.New(t)
+
+	var r d
+	err := Copy2(&r, &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}, s2{"ijkl"}})
+	req.Nil(err)
+	req.Equal("name", r.Name)
+	req.Equal(1234, r.Age)
+	req.Equal(1.85, r.Height)
+	req.Equal("abcd", r.IP.A)
+	req.Equal("efgh", r.I.A)
+	req.Equal("ijkl", r.I2.A)
+}
+
 func BenchmarkNativeCopy(b *testing.B) {
 	r := make([]*d, 0, 50_000_000)
-	src := &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}}
+	src := &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}, s2{"ijkl"}}
 	for b.Loop() {
 		d := &d{
 			Name:   src.Name,
@@ -61,16 +77,21 @@ func BenchmarkNativeCopy(b *testing.B) {
 
 func BenchmarkCopierCopy(b *testing.B) {
 	r := make([]*d, 0, 50_000_000)
-	src := &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}}
-	// f := func(i *s) (*d, error) {
-	// 	var r d
-	// 	if err := Copy(&r, i); err != nil {
-	// 		return nil, err
-	// 	}
-	// 	return &r, nil
-	// }
+	src := &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}, s2{"ijkl"}}
 	for b.Loop() {
 		d, err := Copied[d](src)
+		if err != nil {
+			b.Fatal(err)
+		}
+		r = append(r, d)
+	}
+}
+
+func BenchmarkCopier2Copy(b *testing.B) {
+	r := make([]*d, 0, 50_000_000)
+	src := &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}, s2{"ijkl"}}
+	for b.Loop() {
+		d, err := Copied2[d](src)
 		if err != nil {
 			b.Fatal(err)
 		}
