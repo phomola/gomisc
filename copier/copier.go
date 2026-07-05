56e20532365672d2086bde06c81663e0e6d54437
Petr Homola <homola.p@gmail.com>
2026-07-05 04:22:44 +0100

    more copier field types

--- copier/copier.go
+++ copier/copier.go
@@ -2,6 +2,7 @@ package copier
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"sync"
 	"unsafe"
@@ -30,6 +31,9 @@ func getCopier(dst, src reflect.Type) (func(unsafe.Pointer, unsafe.Pointer) erro
 	}
 	fieldCopiers := make([]func(unsafe.Pointer, unsafe.Pointer) error, 0, src.NumField())
 	for fs := range src.Fields() {
+		if fs.Tag.Get("copier") == "-" {
+			continue
+		}
 		fd, ok := dst.FieldByName(fs.Name)
 		if !ok {
 			return nil, ErrFieldMissing
@@ -44,6 +48,13 @@ func getCopier(dst, src reflect.Type) (func(unsafe.Pointer, unsafe.Pointer) erro
 				*(*int)(dst) = *(*int)(src)
 				return nil
 			})
+		case fs.Type.Kind() == reflect.Float64 && fd.Type.Kind() == reflect.Float64:
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				*(*float64)(dst) = *(*float64)(src)
+				return nil
+			})
 		case fs.Type.Kind() == reflect.String && fd.Type.Kind() == reflect.String:
 			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
 				dst = unsafe.Add(dst, do)
@@ -51,6 +62,25 @@ func getCopier(dst, src reflect.Type) (func(unsafe.Pointer, unsafe.Pointer) erro
 				*(*string)(dst) = *(*string)(src)
 				return nil
 			})
+		case fs.Type.Kind() == reflect.Pointer && fd.Type.Kind() == reflect.Pointer && fs.Type.Elem() == fd.Type.Elem():
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				*(*unsafe.Pointer)(dst) = *(*unsafe.Pointer)(src)
+				return nil
+			})
+		case fs.Type.Kind() == reflect.Struct && fd.Type.Kind() == reflect.Struct:
+			c, err := getCopier(fd.Type, fs.Type)
+			if err != nil {
+				return nil, err
+			}
+			fieldCopiers = append(fieldCopiers, func(dst, src unsafe.Pointer) error {
+				dst = unsafe.Add(dst, do)
+				src = unsafe.Add(src, so)
+				return c(dst, src)
+			})
+		default:
+			return nil, fmt.Errorf("can't create copier for %s -> %s", fs.Type, fd.Type)
 		}
 	}
 	f := func(dst, src unsafe.Pointer) error {
--- copier/copier_test.go
+++ copier/copier_test.go
@@ -9,19 +9,35 @@ import (
 func TestCopy(t *testing.T) {
 	req := require.New(t)
 
+	type d2 struct {
+		A string
+		X bool
+	}
+	type s2 struct {
+		A string
+	}
 	type d struct {
-		Name string
-		Age  int
-		X    bool
+		Name   string
+		Age    int
+		Height float64
+		IP     *s2
+		I      d2
+		X      bool
 	}
 	type s struct {
-		Name string
-		Age  int
+		Name   string
+		Age    int
+		Height float64
+		IP     *s2
+		I      s2
 	}
 
 	var r d
-	err := Copy(&r, &s{"name", 1234})
+	err := Copy(&r, &s{"name", 1234, 1.85, &s2{"abcd"}, s2{"efgh"}})
 	req.Nil(err)
 	req.Equal("name", r.Name)
 	req.Equal(1234, r.Age)
+	req.Equal(1.85, r.Height)
+	req.Equal("abcd", r.IP.A)
+	req.Equal("efgh", r.I.A)
 }
